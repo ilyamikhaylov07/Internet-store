a3981570d7f41e21f0689e0c2dbb9b22ed7b69ad
Egor <egorpenkov2004@gmail.com>
2024-04-08 17:03:37 +0300

    Done login

---
+++ Internet-Store/ApiJson/LoginJson.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Internet_Store.ApiJson
+{
+    public class LoginJson
+    {
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
--- Internet-Store/Controllers/AuthController.cs
+++ Internet-Store/Controllers/AuthController.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Internet_Store.ApiJson;
+using Internet_Store.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 namespace Internet_Store.Controllers
 {
@@ -6,11 +12,26 @@ namespace Internet_Store.Controllers
     [Route("Internetstore/[controller]/[action]")]
     public class AuthController : ControllerBase
     {
-        [HttpGet]
-        
-        public IActionResult Index()
+        [HttpPost]
+        public async  Task<IActionResult> LoginUser(LoginJson user)
         {
-            return Ok();
+            using(AppDbContext context= new AppDbContext()) {
+                var user_ = context.Users.FirstOrDefaultAsync<User>(x=> x.Email==user.Email && x.Password==user.Password);
+                if(user_ is  null)
+                {
+                    return BadRequest("Неправильный логин или пароль");
+                }
+                List<Claim> Claims = new List<Claim>();
+                Claims.Add(new Claim(ClaimTypes.Name, user_.Result.Name));
+                Claims.Add(new Claim(ClaimTypes.Role, "User"));
+                var jwt = new JwtSecurityToken(issuer: AuthOptions.ISSUER,// создание токенов для возвращения метода
+                audience: AuthOptions.AUDIENCE,
+                claims: Claims,
+                expires: DateTime.UtcNow.Add(TimeSpan.FromHours(2)),
+                signingCredentials: new SigningCredentials(AuthOptions.GetSymSecurityKey(), SecurityAlgorithms.HmacSha256));
+                Claims.Add(new Claim(ClaimTypes.Authentication, new JwtSecurityTokenHandler().WriteToken(jwt)));
+                return Ok(new AccesTokenHandler { AccessToken= new JwtSecurityTokenHandler().WriteToken(jwt)});
+            }
         }
     }
 }
