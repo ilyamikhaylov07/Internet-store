baf53788a6d9309653c583075bf9b5e7b07e1f4f
Egor <egorpenkov2004@gmail.com>
2024-04-13 13:07:03 +0300

    Done categories+filter

---
+++ Internet-Store/ApiJsonResponse/Filters.cs
@@ -0,0 +1,11 @@
+﻿namespace Internet_Store.ApiJsonResponse
+{
+    public class Filters
+    {
+        public string? Categorie { get; set; } = null;
+        public string? From { get; set; } = "0";
+        public string? To { get; set; } = "10000000";
+
+
+    }
+}
--- Internet-Store/Controllers/ModelsController.cs
+++ Internet-Store/Controllers/ModelsController.cs
@@ -24,7 +24,7 @@ namespace Internet_Store.Controllers
                     {
                         var modelWithSize = await context.ModelWithSizes.Where(m => m.ModelId == model.Id).ToListAsync();
                         List<string> sizes = new List<string>();
-                        foreach (var modelwithsizes in modelWithSize)    
+                        foreach (var modelwithsizes in modelWithSize)
                         {
                             sizes.Add(modelwithsizes.Size);
                         }
@@ -50,8 +50,79 @@ namespace Internet_Store.Controllers
 
 
         }
+        [HttpPost]
+        public async Task<ActionResult<IEnumerable<ResponseModelWithCatalog>>> CardsWithFIlter(Filters filters)
+        {
+            using (AppDbContext context = new AppDbContext())
+            {
+                if (filters.Categorie == null)
+                {
+
+                    List<ResponseModelWithCatalog> cardmodels = new List<ResponseModelWithCatalog>();
+                    var allmodeles = await context.Models.ToListAsync();
+                    List<Models.Model> models = new List<Models.Model>();
+                    foreach(var model_ in allmodeles)
+                    {
+                        if(int.Parse(model_.Price)>=int.Parse(filters.From) && int.Parse(model_.Price) <= int.Parse(filters.To))
+                        {
+                            models.Add(model_);
+                        }
+                    }
+                    foreach (Models.Model model in models)
+                    {
+                        var modelWithSize = await context.ModelWithSizes.Where(m => m.ModelId == model.Id).ToListAsync();
+                        List<string> sizes = new List<string>();
+                        foreach (var modelwithsizes in modelWithSize)
+                        {
+                            sizes.Add(modelwithsizes.Size);
+                        }
+                        SizeSort.SortSizes(sizes);
+                        cardmodels.Add(new ResponseModelWithCatalog()
+                        {
+                            Name = model.Name,
+                            Image = System.IO.File.ReadAllBytes(Path.Combine(Directory.GetCurrentDirectory(), "Images", model.Image_url)),
+                            Price = model.Price,
+                            Sizes = sizes
+                        });
+                    }
+                    return cardmodels;
+                }
+                else
+                {
+                    List<ResponseModelWithCatalog> cardmodels = new List<ResponseModelWithCatalog>();
+                    var allmodeles = await context.Models.ToListAsync();
+                    List<Models.Model> models = new List<Models.Model>();
+                    foreach (var model_ in allmodeles)
+                    {
+                        if (int.Parse(model_.Price) >= int.Parse(filters.From) && int.Parse(model_.Price) <= int.Parse(filters.To) && 
+                            context.Categories.FirstOrDefault(m=> m.Id==model_.CategoryId).Title==filters.Categorie)
+                        {
+                            models.Add(model_);
+                        }
+                    }
+                    foreach (Models.Model model in models)
+                    {
+                        var modelWithSize = await context.ModelWithSizes.Where(m => m.ModelId == model.Id).ToListAsync();
+                        List<string> sizes = new List<string>();
+                        foreach (var modelwithsizes in modelWithSize)
+                        {
+                            sizes.Add(modelwithsizes.Size);
+                        }
+                        SizeSort.SortSizes(sizes);
+                        cardmodels.Add(new ResponseModelWithCatalog()
+                        {
+                            Name = model.Name,
+                            Image = System.IO.File.ReadAllBytes(Path.Combine(Directory.GetCurrentDirectory(), "Images", model.Image_url)),
+                            Price = model.Price,
+                            Sizes = sizes
+                        });
+                    }
+                    return cardmodels;
+                }
+            }
+
+        }
 
     }
-
 }
 
