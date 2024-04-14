cea4b3a9abbb0a040e3d66139ca48b1054232240
Ilya <mr.ilmix@mail.ru>
2024-04-14 19:58:30 +0300

    Added Profile page and controller_profile_page

---
+++ Internet-Store/ApiJsonResponse/ResponseUserLK.cs
@@ -0,0 +1,15 @@
+﻿namespace Internet_Store.ApiJsonResponse
+{
+    public class ResponseUserLK
+    {
+        public required int Id { get; set; }
+        public required string Name { get; set; }
+        public required string NumberPhone { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+        public required string? EmailToken { get; set; }
+        public required bool IsConfirmed { get; set; }
+
+
+    }
+}
