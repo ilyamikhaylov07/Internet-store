0a45ac046b16f021ac3f36b2cee981f72962ff8d
Ilya <mr.ilmix@mail.ru>
2024-04-16 18:11:51 +0300

    Added AddNewModelAdminPage

--- Internet-Store-frontEnd/vite-project/src/App.tsx
+++ Internet-Store-frontEnd/vite-project/src/App.tsx
@@ -1,8 +1,7 @@
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 import MainBar from './NavBar';
 import Registration from './Registration';
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './Home';
 import CatalogPage from './Catalog';
 import { useEffect, useState } from 'react';
@@ -11,55 +10,54 @@ import FilteredPage from './FilteredPage';
 import AdminDashboard from './AdminDashboard';
 import AdminProfile from './AdminPages/AdminProfile';
 import ModelPage from './ModelPage';
+import AdminAddNewModelPage from './AdminPages/AdminAddNewModel';
 
-function App(){
+function App() {
+  const [selectedTab, setSelectedTab] = useState(null);
+
   const [isLoggedIn, setIsLoggedIn] = useState(() => {
     const accessToken = localStorage.getItem('accessToken');
     const isUser = localStorage.getItem('isUser') === 'true';
     return accessToken !== null && isUser;
   });
-  
+
   const [isLoggedInAdmin, setIsLoggedInAdmin] = useState(() => {
     const accessToken = localStorage.getItem('accessToken');
     const isAdmin = localStorage.getItem('isAdmin') === 'true';
     return accessToken !== null && isAdmin;
   });
+
   const handleLogoutUser = () => {
     localStorage.removeItem('accessToken');
     setIsLoggedIn(false);
   };
-  const handleLogoutAdmin = () => { // Функция для выхода администратора
+
+  const handleLogoutAdmin = () => {
     localStorage.removeItem('accessToken');
     setIsLoggedInAdmin(false);
   };
-  
-  return (
-      <Router>
-      <MainBar isLoggedIn={isLoggedIn} handleLogoutUser={handleLogoutUser} isLoggedInAdmin={isLoggedInAdmin} handleLogoutAdmin={handleLogoutAdmin}/>
-      <Routes>
 
-        <Route path="/home" element={<HomePage/>} />
+  return (
+    <Router>
+      <MainBar
+        isLoggedIn={isLoggedIn}
+        handleLogoutUser={handleLogoutUser}
+        isLoggedInAdmin={isLoggedInAdmin}
+        handleLogoutAdmin={handleLogoutAdmin}
+      />
+      <Routes>
+        <Route path="/home" element={<HomePage />} />
         <Route path="/login" element={<Registration setIsLoggedIn={setIsLoggedIn} setIsLoggedInAdmin={setIsLoggedInAdmin} />} />
-        <Route path="/catalog" element={<CatalogPage/>}/>
-        <Route path="/profile" element={<Profile/>}/>
-        <Route path="/catalog/filtered?" element={<FilteredPage/>}/>
-        {isLoggedInAdmin && (
-          <Route path="/admin-dashboard" element={<AdminDashboard />} /> 
-          
-        )}
-        {isLoggedInAdmin && (
-          <Route path="/profile-admin" element={<AdminProfile/>}/>
-        )}
-        <Route path="/catalog/id?" element={<ModelPage/>}/>
-        
-        
+        <Route path="/catalog" element={<CatalogPage />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/catalog/filtered?" element={<FilteredPage />} />
+        {isLoggedInAdmin && <Route path="/admin-dashboard/*" element={<AdminDashboard selectedTab={selectedTab} setSelectedTab={setSelectedTab} />} />}
+        {isLoggedInAdmin && <Route path="/profile-admin" element={<AdminProfile />} />}
+        {isLoggedInAdmin && <Route path="/admin-dashboard/add-product" element={<AdminAddNewModelPage />} />}
+        <Route path="/catalog/id?" element={<ModelPage />} />
       </Routes>
     </Router>
-  
-    
   );
-  
-  
 }
 
 export default App;
