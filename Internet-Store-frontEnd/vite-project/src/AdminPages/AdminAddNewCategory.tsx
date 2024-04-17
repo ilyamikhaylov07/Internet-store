d5e215ecfed543086b2b55aa77fafb95f75e311c
Ilya <mr.ilmix@mail.ru>
2024-04-17 19:29:29 +0300

    Space

--- Internet-Store-frontEnd/vite-project/src/AdminPages/AdminAddNewCategory.tsx
+++ Internet-Store-frontEnd/vite-project/src/AdminPages/AdminAddNewCategory.tsx
@@ -3,24 +3,24 @@ import AdminDashboard from "../AdminDashboard";
 import { useState } from "react";
 import axios from "axios";
 
-function AdminAddNewCategoryPage(){
-    
+function AdminAddNewCategoryPage() {
+
     const [selectedTab, setSelectedTab] = useState(null); // Управление выбранной вкладкой
-    
+
     const [formData, setFormData] = useState({
         title: '',  // Начальное значение для одной пары размера и количества
     });
 
-    const SaveHandler = async (event : any) => {
+    const SaveHandler = async (event: any) => {
         const token = localStorage.getItem('accessToken');
-        const headers ={
+        const headers = {
             'Content-Type': 'application/json',
             'Authorization': `Bearer ${token}`
         }
         event.preventDefault();
         try {
             // Отправка данных на сервер
-            const response = await axios.post('https://localhost:7239/Internetstore/Admin/AddCategory', formData, {headers});
+            const response = await axios.post('https://localhost:7239/Internetstore/Admin/AddCategory', formData, { headers });
             console.log('Ответ от сервера:', response.data);
             // Очистка формы после успешной отправки
             setFormData({
@@ -34,29 +34,29 @@ function AdminAddNewCategoryPage(){
     };
 
 
-    return(
+    return (
         <>
             <AdminDashboard selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
-            <div style={{ marginTop: '100px', marginLeft: '500px',maxWidth: '800px' , justifyContent: 'center', alignItems: 'center'}}>
-                
-            <h1>Добавление новой категории товара</h1>
-            <Form onSubmit={SaveHandler}>
-                <FloatingLabel controlId="Title" label="Название категории" className="mb-3">
-                    <Form.Control
-                        style={{ width: '100%' }}
-                        type="text"
-                        name="title"
-                        value={formData.title}
-                        onChange={(event) => setFormData({ ...formData, title: event.target.value })}
-                    />
-                </FloatingLabel>
-                    
+            <div style={{ marginTop: '100px', marginLeft: '500px', maxWidth: '800px', justifyContent: 'center', alignItems: 'center' }}>
 
-                <Button type="submit" style={{marginLeft: '700px'}} onClick={SaveHandler}>
-                    Сохранить
-                </Button>
-            </Form>
-        </div>
+                <h1>Добавление новой категории товара</h1>
+                <Form onSubmit={SaveHandler}>
+                    <FloatingLabel controlId="Title" label="Название категории" className="mb-3">
+                        <Form.Control
+                            style={{ width: '100%' }}
+                            type="text"
+                            name="title"
+                            value={formData.title}
+                            onChange={(event) => setFormData({ ...formData, title: event.target.value })}
+                        />
+                    </FloatingLabel>
+
+
+                    <Button type="submit" style={{ marginLeft: '700px' }} onClick={SaveHandler}>
+                        Сохранить
+                    </Button>
+                </Form>
+            </div>
         </>
     )
 }
