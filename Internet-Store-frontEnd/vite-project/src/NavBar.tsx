087f777a967ad0aefcf0ef0c3852cb0b62da7c5f
Ilya <mr.ilmix@mail.ru>
2024-04-18 18:27:06 +0300

    Added AllOrdersPageAdmin and controller with

--- Internet-Store-frontEnd/vite-project/src/NavBar.tsx
+++ Internet-Store-frontEnd/vite-project/src/NavBar.tsx
@@ -17,7 +17,6 @@ function MainBar({isLoggedIn, handleLogoutUser, isLoggedInAdmin, handleLogoutAdm
             <Nav.Link as={Link} to="/blog">Блог</Nav.Link>
             <Nav.Link as={Link} to="/about">О нас</Nav.Link>
             <Nav.Link as={Link} to="/FAQ">FAQ</Nav.Link>
-            <Nav.Link as={Link} to="/delivery-and-payment">Доставка и оплата</Nav.Link>
             {/* Страницы для администратора */}
             </Nav>
             <Nav className="ms-auto">
